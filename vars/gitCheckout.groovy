810009158ab5b7cfd394781a8cd12cc913cf9ddc
ketanonline <ketanonline@gmail.com>
2024-01-22 21:02:35 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -24,27 +24,27 @@ def call(Map pipelineParams) {
                 }
             }
 
-            stage('deploy developmentServer'){
-                steps {
-                    deploy(pipelineParams.developmentServer, pipelineParams.serverPort)
-                }
-            }
+            // stage('deploy developmentServer'){
+            //     steps {
+            //         deploy(pipelineParams.developmentServer, pipelineParams.serverPort)
+            //     }
+            // }
 
-            stage('deploy staging'){
-                steps {
-                    deploy(pipelineParams.stagingServer, pipelineParams.serverPort)
-                }
-            }
+            // stage('deploy staging'){
+            //     steps {
+            //         deploy(pipelineParams.stagingServer, pipelineParams.serverPort)
+            //     }
+            // }
 
-            stage('deploy production'){
-                steps {
-                    deploy(pipelineParams.productionServer, pipelineParams.serverPort)
-                }
-            }
+            // stage('deploy production'){
+            //     steps {
+            //         deploy(pipelineParams.productionServer, pipelineParams.serverPort)
+            //     }
+            // }
         }
         post {
             failure {
-                mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
+                // mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
             }
         }
     }
