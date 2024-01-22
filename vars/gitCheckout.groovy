d5f05802ad106c9cdd7b1aff98670e5cd658a059
ketanonline <ketanonline@gmail.com>
2024-01-22 21:01:42 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,6 +1,51 @@
-def call(String repoUrl, String branch){
-   def workingDir = "${env.WORKSPACE}"
-   sh "git clone ${repoUrl} ${workingDir}"
-   sh "git checkout ${branch}"
-   return workingDir
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                    git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl
+                }
+            }
+
+            stage('build') {
+                steps {
+                    sh 'mvn clean package -DskipTests=true'
+                }
+            }
+
+            stage ('test') {
+                steps {
+                    parallel (
+                        "unit tests": { sh 'mvn test' },
+                        "integration tests": { sh 'mvn integration-test' }
+                    )
+                }
+            }
+
+            stage('deploy developmentServer'){
+                steps {
+                    deploy(pipelineParams.developmentServer, pipelineParams.serverPort)
+                }
+            }
+
+            stage('deploy staging'){
+                steps {
+                    deploy(pipelineParams.stagingServer, pipelineParams.serverPort)
+                }
+            }
+
+            stage('deploy production'){
+                steps {
+                    deploy(pipelineParams.productionServer, pipelineParams.serverPort)
+                }
+            }
+        }
+        post {
+            failure {
+                mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
+            }
+        }
+    }
 }
