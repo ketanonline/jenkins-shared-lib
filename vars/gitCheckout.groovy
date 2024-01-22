be6b00f8106931e7ef1823a3da855ebb0e7e6256
ketanonline <ketanonline@gmail.com>
2024-01-22 22:21:39 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,11 +1,7 @@
-def call(body) {
+def call(Map pipelineParams) {
 
     // evaluate the body block, and collect configuration into the object
-    def pipelineParams= [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = pipelineParams
-    body()
-    
+
     pipeline {
         agent any
         stages {
